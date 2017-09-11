21ac23b2587619685e042b63a6c6c6cc4099a813
Jashua R. White <jashuawhite@gmail.com>
2017-09-10 22:07:20 -0400

    Updates to code design pattern and structure

--- js/main.js
+++ js/main.js
@@ -4,98 +4,48 @@ var calculator = (function() {
 	var DEBUG_ON = 1;
 
 	function CalculatorModel() {
-		consoleLog('Model: initialized'); // debug
+		consoleLog('MODEL: initialized'); // Debug
 
 		this.numberStack = [];
-		this.operatorStack = [];
 		this.calculateStack = [];
-		this.resultStack = [];
 
-		this.numberAdded = new Event(this); // sender model
-		this.operatorAdded = new Event(this);
-		this.resultCalculated = new Event(this);
+		this.numberValuePushed = new Event(this);
+		this.operatorValuePushed = new Event(this);
 
 		var _self = this;
-
-		this.add = function(a, b) {
-			var result = a + b;
-			return result;
-		}
 	}
-
 	CalculatorModel.prototype = {
-		// number
-		storeNumber: function(value) {
-			consoleLog('Model: model.storeNumber(' + value + ') > numberStack.push(' + value + ')');
-			consoleLog('Model: model.storeNumber(' + value + ') > numberAdded.notify()');
-			this.numberStack.push(value);
-			if (this.operatorStack[0] !== '' && this.operatorStack[0]) {
-				consoleLog('Push calc stack');
-				this.calculateStack.push(this.getOperator());
-			}
-			this.operatorStack = [];
-			this.numberAdded.notify({ 'number' : value });
+		// Number
+		pushNumber: function(number) {
+			this.numberStack.push(number);
+			this.numberValuePushed.notify({ 'number' : this.getNumber() });
 		},
 		getNumber: function() {
 			return this.numberStack.join('');
 		},
-		// operator
-		storeOperator: function(value) {
-			if (this.getOperator() === value) return;
-
-			consoleLog('Model: model.storeOperator(' + value + ')');
-			this.setOperator(0, value);
-			consoleLog('Array length: ' + this.calculateStack.length);
-			consoleLog('Array[0]: ' + this.calculateStack[0]);
-			if (this.calculateStack.length < 1) {
-				consoleLog('Added to calculafte stack[0]: ' + this.getNumber());
-				this.calculateStack[0] = this.getNumber();
-			} else {
-				consoleLog('Pushed to calculate stack: ' + this.getNumber());
-				this.calculateStack.push(this.getNumber());
-			}
+		clearNumber: function() {
 			this.numberStack = [];
-			this.operatorAdded.notify({ 'operator' : value });
-
 		},
-		getOperator: function() {
-			return this.operatorStack[0];
+		// Calculate
+		pushCalculate: function(value) {
+			this.calculateStack.push(value);
+			this.operatorValuePushed.notify({ 'operator' : this.popCalculate() });
 		},
-		setOperator: function(index, value) {
-			this.operatorStack[index] = value;
+		popCalculate: function(bool) {
+			if (bool === true)
+				return this.calculateStack.pop();
+			if (this.calculateStack.length > 0)
+				return this.calculateStack[this.calculateStack.length - 1];
 		},
-		// result
-		getResult: function() {
-			return this.resultStack[this.resultStack.length - 1];
+		clearCalculate: function() {
+			this.calculateStack = [];
 		},
-		pushResult: function(value) {
-			this.resultStack.push(value);
-		},
-		// calculate
 		calculateResult: function() {
-
-		},
-		getCalculateResult: function() {
-			return this.calculateStack[2];
-		},
-		addCalculateResult: function() {
-			if (this.calculateStack.length < 3) return;
-
-			consoleLog('calculateResult()');
-			consoleLog(this.calculateStack[0]);
-			consoleLog(this.calculateStack[1]);
-			consoleLog(this.calculateStack[2]);
-			var result = 0; // number
-
-			switch (this.calculateStack[1]) {
-				case '+':
-					consoleLog('Calculate: ' + parseInt(this.calculateStack[0]));
-					result = parseInt(this.calculateStack[0]) + parseInt(this.calculateStack[2]);
-					consoleLog('Calculate result = ' + result);
-					break;
-			}
-			this.calculateStack.push(result);
-			this.resultCalculated.notify({ 'result' : this.getCalculatedResult() });
+			var result = 0;
+			// for (var i = 0; i < this.calculateStack.length; i++) {
+			//
+			// }
+			console.log(this.calculateStack);
 		}
 	};
 
@@ -105,56 +55,39 @@ var calculator = (function() {
     * events to handle the user interaction.
     */
 	function CalculatorView(model, elements) {
-		consoleLog('View: initialized'); // debug
+		consoleLog('VIEW: initialized'); // Debug
 
 		this._model = model;
-		this._$elements = elements;
+		this._elements = elements;
+		this._numberButtonElements = elements.number;
+		this._operatorButtonElements = elements.operator;
+		this._displayElement = elements.display;
 
-		this.numberButtonClicked = new Event(this); // sender view
-		this.operatorButtonClicked = new Event(this); // sender view
+		this.numberButtonClicked = new Event(this);
+		this.operatorButtonClicked = new Event(this);
 
 		var _self = this;
 
-		// attach model listeners
-		this._model.numberAdded.attach(function(sender, args) {
-			console.log('Model Listener: model.numberAdded() > view.renderDisplay()')
-			_self.renderDisplay(args);
-		});
-		this._model.operatorAdded.attach(function(sender, args) {
-			_self.renderDisplay(args);
-		});
-		this._model.resultCalculated.attach(function(sender, args) {
-			consoleLog("Result calc");
-			_self.renderDisplay(args);
-		});
-
 		// attach listeners to HTML controls
-		//this._elements.display.change(function() {
-		//	_self.displayChanged.notify();
-		//});
-		this._$elements.number.click(function() {
-			consoleLog('View: _$elements.number.click() > view.numberButtonClicked.notify(' + $(this).html() + ')');
+		this._numberButtonElements.click(function() {
 			_self.numberButtonClicked.notify({ 'number' : $(this).html() });
 		});
-		this._$elements.operator.click(function() {
-			consoleLog('View: _$elements.operator.click() > view.operatorButtonClicked.notify(' + $(this).html() + ')');
+
+		this._operatorButtonElements.click(function() {
 			_self.operatorButtonClicked.notify({ 'operator' : $(this).html() });
 		});
 	}
 	CalculatorView.prototype = {
 		renderDisplay: function(args) {
 			if (args.hasOwnProperty('number')) {
-				consoleLog('View: view.renderDisplay() > display rendered'); // debug
-				this._$elements.display.html(this._model.getNumber());
+				this._displayElement.html(args.number);
 			}
 			if (args.hasOwnProperty('operator')) {
-				consoleLog('View: view.renderDisplay() > display rendered'); // debug
-				this._$elements.display.html(this._model.getOperator());
+				this._displayElement.html(args.operator);
 			}
-			if (args.hasOwnProperty('result')) {
-				consoleLog('View: view.renderDisplay() > display rendered'); // debug
-				this._$elements.display.html(this._model.getResult());
-			}
+		},
+		clearDisplay: function() {
+			this._displayElement.html('0');
 		}
 	};
 
@@ -163,40 +96,55 @@ var calculator = (function() {
     * invokes changes on the model.
     */
 	function CalculatorController(model, view) {
-		consoleLog('Controller: initialized'); // debug
+		consoleLog('CONTROLLER: initialized'); // debug
 
 		this._model = model;
 		this._view = view;
 
 		var _self = this;
 
+		// attach model listeners
+		this._model.numberValuePushed.attach(function(sender, args) {
+			_self._view.renderDisplay(args);
+		});
+
+		this._model.operatorValuePushed.attach(function(sender, args) {
+			_self._view.renderDisplay(args);
+		});
+
+		// attach view listeners
 		this._view.numberButtonClicked.attach(function(sender, args) {
-			console.log('View Listener: view.numberButtonClicked() > controller.storeNumber(' + args.number + ')');
 			_self.numberPressed(args.number);
 		});
+
 		this._view.operatorButtonClicked.attach(function(sender, args) {
-			console.log('View Listener: view.operatorButtonClicked() > controller.storeOperator(' + args.operator+ ')');
 			_self.operatorPressed(args.operator);
 		});
 	}
 	CalculatorController.prototype = {
 		numberPressed: function(number) {
-			consoleLog('Controller: model.storeNumber(' + number + ') > number added to model');
-			this._model.storeNumber(number);
+			this._model.pushNumber(number);
 		},
 		operatorPressed: function(operator) {
-			if (this._model.getOperator() === operator) return;
-
-			switch (operator) {
-				case '=':
-					consoleLog('equals button ' + this._model.getCalculatedResult());
-					this._model.addCalculatedResult();
-					break;
-				default:
-					consoleLog('Controller: model.operatorPressed > model.storeOperator');
-					this._model.storeOperator(operator);
-					break;
+			// If not empty, add the number to calculate stack and then clear the number stack
+			if (this._model.getNumber().length > 0 ) {
+				this._model.pushCalculate(this._model.getNumber());
+				this._model.clearNumber();
 			}
+			if (this._model.popCalculate() !== operator) {
+				this._model.pushCalculate(operator);
+			}
+			if (operator === '=') {
+				this._model.calculateResult();
+				this.clearCalculator();
+			} else if (operator === 'C') {
+				this.clearCalculator();
+			}
+		},
+		clearCalculator: function() {
+			this._model.clearNumber();
+			this._model.clearCalculate();
+			this._view.clearDisplay();
 		}
 	};
 
@@ -207,7 +155,6 @@ var calculator = (function() {
 		this._sender = sender;
 		this._listeners = [];
 	}
-
 	Event.prototype = {
 		attach : function (listener) {
 			this._listeners.push(listener);
@@ -216,11 +163,6 @@ var calculator = (function() {
 			var index;
 
 			for (index = 0; index < this._listeners.length; index += 1) {
-				//consoleLog('Event.notify() sender:');
-				//consoleLog(this._sender);
-				//consoleLog('Event.notify() listener: ');
-				//consoleLog(this._listeners[index]);
-				//consoleLog(args);
 				this._listeners[index](this._sender, args);
 			}
 		}
@@ -236,7 +178,6 @@ var calculator = (function() {
 			'operator' : $('.js-calculator .btn.operator')
 		});
 		var controller = new CalculatorController(model, view);
-		//view.show();
 	});
 
 	/**
